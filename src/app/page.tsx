5bda1d2d6fc6fbacc8067fe01aa48788db6f6a48
eujin-shin <eujinshin0124@gmail.com>
2024-01-17 01:32:00 +0900

    feat: init project

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import { styled } from "styled-components";
+
+export default function Home() {
+  return (
+    <div
+      style={{
+        backgroundColor: "#ECE5D9",
+        height: "100%",
+        width: "100%",
+        minWidth: 430,
+        minHeight: 700,
+        position: "absolute",
+      }}
+    ></div>
+  );
+}
