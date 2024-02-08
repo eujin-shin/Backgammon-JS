ce403e984cf251c35df5c6c477c1fa4749452185
eujin-shin <eujinshin0124@gmail.com>
2024-02-08 21:00:41 +0900

    feat: set theme variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,25 @@
+"use client";
+
+import Board from "@/components/Board";
+import Turn from "@/components/Turn";
 import Image from "next/image";
 import { styled } from "styled-components";
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  @media screen and (min-width: 700px) {
+    flex-direction: row;
+    justify-content: space-between;
+  }
+`;
+
 export default function Home() {
   return (
-    <div
-      style={{
-        backgroundColor: "black",
-        height: "100%",
-        width: "100%",
-      }}
-    ></div>
+    <Wrapper>
+      <Board />
+      <Turn />
+    </Wrapper>
   );
 }
