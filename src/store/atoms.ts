be6a26b57a9cf4eed0598dc360e24b0079931ff8
eujin-shin <eujinshin0124@gmail.com>
2024-03-12 22:47:55 +0900

    feat: add playing global state

--- src/store/atoms.ts
+++ src/store/atoms.ts
@@ -2,7 +2,20 @@ import { atom } from "recoil";
 
 type phaseType = "init" | "user" | "com";
 
+type playType = {
+  current: "waiting" | "rolled";
+  dices: number[];
+};
+
 export const phaseState = atom<phaseType>({
   key: "phase",
   default: "init",
+});
+
+export const playState = atom<playType>({
+  key: "play",
+  default: {
+    current: "waiting",
+    dices: [],
+  },
 });
